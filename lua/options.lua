4cae63efa60cf3821d3acd39ad36f1eaf7d31add
Nguyen Thien Duc <ntduc4@gmail.com>
2025-01-13 19:22:31 +0800

    setting init

---
+++ lua/options.lua
@@ -0,0 +1,9 @@
+require "nvchad.options"
+
+-- add yours here!
+
+local o = vim.o
+o.cursorlineopt = "both" -- to enable cursorline!
+o.rnu = true
+
+vim.g.vscode_snippets_path = "~/.config/nvim/snippets"
---
+++ lua/plugins/init.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+  --
+  -- {
+  --   "L3MON4D3/LuaSnip",
+  --   config = function()
+  --     require("luasnip.loaders.from_vscode").lazy_load {
+  --       paths = "~/.config/nvim/snippets",
+  --     }
+  --   end,
+  -- },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
